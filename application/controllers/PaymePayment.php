7d7e38923df67ed308b35dc63e4c327a9a36be8e
octopuscart <dev@develop.com>
2021-06-02 21:18:14 +0530

    chagns

--- application/controllers/PaymePayment.php
+++ application/controllers/PaymePayment.php
@@ -250,7 +250,7 @@ class PaymePayment extends CI_Controller {
             "notificationUri" => $notificatonurl,
             "appSuccessCallback" => $successurl,
             "appFailCallback" => $failureurl,
-            "effectiveDuration" => 15,
+            "effectiveDuration" => 60,
             "merchantData" => array(
                 "orderId" => $orderno,
             ),
